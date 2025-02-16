9efabc0ee00be6c473adb7d8a8639efc8a279d8f
Irish Rixon Dela Pena <irishrixon21delapena@outlook.com>
2025-02-16 20:01:34 +0800

    `Added email verification functionality and verification code endpoint`

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,8 @@
 const { UsersModel } = require("../models/users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 require("dotenv").config();
 
 const signUp = async (req, res) => {
@@ -43,32 +45,80 @@ const signIn = async (req, res) => {
                 } else if (result) {
                     const jwtToken = jwt.sign({ email }, process.env.JWT_SECRETKEY);
 
-                    res.cookie('token', jwtToken, {
+                    res.cookie("token", jwtToken, {
                         httpOnly: true,
-                        secure: true
-                    })
-                    console.log('Cookie: ', res.get('Set-Cookie'));
+                        secure: true,
+                    });
+                    console.log("Cookie: ", res.get("Set-Cookie"));
                     res.status(200).json({ isLoggedIn: true });
                 } else {
                     res.status(200).json({ message: "Incorrect Password" });
                 }
             });
-        }
-        else {
-            res.json({ message: 'Account not exist' })
+        } else {
+            res.json({ message: "Account not exist" });
         }
     } catch (error) {
         console.log(error);
     }
 };
 
+let verificationCode;
 const emailVerification = async (req, res) => {
-    console.log(req.body);
-    res.status(200).json(req.body)
+    const userEmail = req.body.email;
+    let min = 100000;
+    let max = 1000000;
+    verificationCode = crypto.randomInt(min, max);
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "pawfile.official@gmail.com",
+            pass: process.env.GMAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: "pawfile.official@gmail.com",
+        to: userEmail,
+        subject: "Verify Your Email Address for PawFie",
+        text: `Greetings PawFriend,
+
+Thank you for registering with PawFile! To complete your sign-up process, please verify your email address by entering the verification code below:
+
+Verification Code: ${verificationCode}
+
+If you didn't request this, please ignore this email.
+
+Cheers,
+The PawFile Team
+`,
+    }; //for enabling your google account to send email, got to your google acount settings and search app password
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json({ message: "Error sending email" });
+        } else {
+            console.log("Email sent: " + info.response);
+            res.status(200).json({ message: "Email sent" });
+        }
+    });
+};
+
+const verifyCode = async (req, res) => {
+    const { code } = req.body;
+    if(code == verificationCode){
+        res.status(200).json({ isMatch: true });
+    }
+    else {
+        res.status(200).json({ isMatch: false });
+    }
 }
 
 module.exports = {
     signUp,
     signIn,
-    emailVerification
+    emailVerification,
+    verifyCode
 };
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,12 +3,14 @@ const authenticationRouter = express.Router();
 const {
     signUp,
     signIn,
-    emailVerification
+    emailVerification,
+    verifyCode
 } = require('../controllers/authentication');
 
 authenticationRouter.route('/signup').post(signUp);
 authenticationRouter.route('/signin').post(signIn);
 authenticationRouter.route('/emailverif').post(emailVerification);
+authenticationRouter.route('/verifcode').post(verifyCode);
 // authenticationRouter.route('google-signIn').post()
 
 module.exports = authenticationRouter;
