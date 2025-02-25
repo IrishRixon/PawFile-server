f32cd8b9700c654dbb4928c5d9f109d0428e863a
Irish Rixon Dela Pena <irishrixon21delapena@outlook.com>
2025-02-25 08:49:33 +0800

    `Added new authentication routes and functions: findAccount and changePassword`

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,13 +4,17 @@ const {
     signUp,
     signIn,
     emailVerification,
-    verifyCode
+    verifyCode,
+    findAccount,
+    changePassword
 } = require('../controllers/authentication');
 
 authenticationRouter.route('/signup').post(signUp);
 authenticationRouter.route('/signin').post(signIn);
 authenticationRouter.route('/emailverif').post(emailVerification);
 authenticationRouter.route('/verifcode').post(verifyCode);
+authenticationRouter.route('/findaccount').post(findAccount);
+authenticationRouter.route('/changePassword').put(changePassword);
 // authenticationRouter.route('google-signIn').post()
 
 module.exports = authenticationRouter;
