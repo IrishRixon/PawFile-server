bc6b2354dacce60e10c950d546e76e4e76555f0d
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-05-01 20:23:25 +0800

    `Removed petProfileDetails object and moved medicalDetails to response directly`

--- controllers/dashboard/petProfileDetails.js
+++ controllers/dashboard/petProfileDetails.js
@@ -10,14 +10,12 @@ const getPetProfileDetails = async (req, res) => {
         const ownerDetails = await UserIFormModel.findOne({ email });
         const medicalDetails = await MedicalIFormModel.findOne({ petName: name });
 
-        const petProfileDetails = {
+        console.log(name);
+        res.status(200).json({ 
             petDetails,
             ownerDetails,
-            medicalDetails
-        }
-
-        console.log(name);
-        res.status(200).json({ petProfileDetails });
+            medicalDetails 
+        });
     } catch (error) {
         res.status(400).json({ error });
     }
