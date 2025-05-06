5816e2bffaea78e370d6685a998d28c8a342ec8a
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-05-06 22:16:50 +0800

    `Added updatePetDetails function and route to petProfileDetails controller and router`

--- controllers/dashboard/petProfileDetails.js
+++ controllers/dashboard/petProfileDetails.js
@@ -1,6 +1,6 @@
-const PetIFormModel = require('../../models/petInitialForm');
-const UserIFormModel = require('../../models/userInitialForm');
-const MedicalIFormModel = require('../../models/medicalIForm');
+const PetIFormModel = require("../../models/petInitialForm");
+const UserIFormModel = require("../../models/userInitialForm");
+const MedicalIFormModel = require("../../models/medicalIForm");
 
 const getPetProfileDetails = async (req, res) => {
     try {
@@ -8,18 +8,47 @@ const getPetProfileDetails = async (req, res) => {
         const { email } = req.user;
         const petDetails = await PetIFormModel.findOne({ _id: id });
         const ownerDetails = await UserIFormModel.findOne({ email });
-        const medicalDetails = await MedicalIFormModel.findOne({ petName: petDetails.name });
+        const medicalDetails = await MedicalIFormModel.findOne({
+            petName: petDetails.name,
+        });
 
-        res.status(200).json({ 
+        res.status(200).json({
             petDetails,
             ownerDetails,
-            medicalDetails 
+            medicalDetails,
         });
     } catch (error) {
         res.status(400).json({ error });
     }
-}
+};
+
+const updatePetDetails = async (req, res) => {
+    try {
+        const newData = req.body;
+        const { email } = req.user;
+        const { name } = newData;
+        console.log(newData);
+        console.log(name);
+
+        const petDetails = await PetIFormModel.findOneAndUpdate(
+            {
+                name,
+                owner: email,
+            },
+            newData,
+            {
+                new: true
+            }
+        );
+        console.log(petDetails);
+        res.status(200).json(petDetails);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('An error occured.');
+    }
+};
 
 module.exports = {
     getPetProfileDetails,
-}
+    updatePetDetails,
+};
--- routes/dashboard/petProfileDetails.js
+++ routes/dashboard/petProfileDetails.js
@@ -1,8 +1,10 @@
 const petProfileDetailsRouter = require('express').Router();
 const {
     getPetProfileDetails,
+    updatePetDetails
 } = require('../../controllers/dashboard/petProfileDetails');
 
 petProfileDetailsRouter.route('/getPetProfileDetails/:id').get(getPetProfileDetails);
+petProfileDetailsRouter.route('/updatePetDetails').put(updatePetDetails);
 
 module.exports = petProfileDetailsRouter;
