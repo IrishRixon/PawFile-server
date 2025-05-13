ad5a10013e936346e3879855d4259767f95ba8e5
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-05-13 18:30:38 +0800

    `Added single image upload functionality to pet profile details routes`

--- routes/dashboard/petProfileDetails.js
+++ routes/dashboard/petProfileDetails.js
@@ -8,12 +8,16 @@ const {
     updateNameDetails
 } = require('../../controllers/dashboard/petProfileDetails');
 
+const { singleImageUpload } = require('../../controllers/singleImageUpload');
+const upload = require('../../utils/multer');
+
 petProfileDetailsRouter.route('/getPetProfileDetails/:id').get(getPetProfileDetails);
 petProfileDetailsRouter.route('/updatePetDetails').put(updatePetDetails);
 petProfileDetailsRouter.route('/updateMedicalDetailsForm').put(updateMedicalDetails);
 petProfileDetailsRouter.route('/updateOwnerDetails').put(updateOwnerDetails);
 petProfileDetailsRouter.route('/updateMessageDetails').put(updateMessageDetails);
 petProfileDetailsRouter.route('/updateNameDetails').put(updateNameDetails);
+petProfileDetailsRouter.route('/uploadPetImage').put(upload.single('image'), singleImageUpload);
 
 
 module.exports = petProfileDetailsRouter;
